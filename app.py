13d7fa0bd73551c57226127833134563926f9be3
violet <101194343+joococo@users.noreply.github.com>
2024-04-23 10:52:32 +0900

    sever complete

--- app.py
+++ app.py
@@ -1,108 +1,142 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 import requests
-import uuid
 import json
+from PIL import Image, ImageDraw, ImageFont
+import base64
+from io import BytesIO
+import uuid
 import time
-import os
+
 
 app = Flask(__name__)
 
-# 업로드 된 이미지에서 글자 인식 후 반환
+
+# 진짜 코드
 # @app.route('/upload', methods=['POST', 'GET'])
 # def process_image():
-#     api_url = 'https://eoxy3e0azd.apigw.ntruss.com/custom/v1/29612/cb56e51bd632826fb10645cf94923e9934c4789df11256f9baaf17de98b0e7f9/general'
-#     secret_key = 'VG1ocGpZcXlDU2xuRWFRaU1XdEtWWm1JSlZxeVV2c1A='
-#     headers = {
-#         'X-OCR-SECRET': secret_key
-#     }
-#     path = "./image_test/gamja.jpg"
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         if file:
+#             # API 설정
+#             api_url = 'https://eoxy3e0azd.apigw.ntruss.com/custom/v1/29612/cb56e51bd632826fb10645cf94923e9934c4789df11256f9baaf17de98b0e7f9/general'
+#             secret_key = 'VG1ocGpZcXlDU2xuRWFRaU1XdEtWWm1JSlZxeVV2c1A='
+#             headers = {'X-OCR-SECRET': secret_key}
 #
-#     # 파일을 여는 방법을 변경
-#     if not os.path.exists(path):
-#         return jsonify({'error': 'File not found'}), 404
-#
-#     files = {'file': open(path, 'rb')}
-#     request_json = {
-#         'images': [{'format': 'jpg', 'name': 'demo'}],
-#         'requestId': str(uuid.uuid4()),
-#         'version': 'V2',
-#         'timestamp': int(round(time.time() * 1000))
-#     }
-#     payload = {'message': json.dumps(request_json).encode('UTF-8')}
-#
-#     try:
-#         # 파일과 함께 페이로드 전송
-#         response = requests.post(api_url, headers=headers, files=files, data=payload)
-#         response.raise_for_status()  # 요청 실패 시 예외 발생
-#         result = response.json()
-#         text_results = ' '.join([field['inferText'] for field in result['images'][0]['fields']])
-#         return jsonify({'text': text_results})
-#     except requests.exceptions.RequestException as e:
-#         return jsonify({'error': str(e)}), 500
-#     finally:
-#         files['file'].close()  # 파일 핸들을 명시적으로 닫음
+#             # 이미지 로드
+#             image = Image.open(file.stream)
+#             draw = ImageDraw.Draw(image)
+#             font_path = './Font/Korail_Round_Gothic_Bold.ttf'
+#             font = ImageFont.truetype(font_path, 16)
 #
 #
-# # 이미지 업로드 된 거 받아오기
-# UPLOAD_FOLDER = '/path/to/the/uploads'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#             # 파일을 메모리에서 직접 읽어 API로 전송
+#             file.seek(0)  # 스트림 위치를 처음으로 되돌림
+#             files = {'file': (file.filename, file, file.content_type)}
 #
-# @app.route('/imageupload', methods=['POST'])
-# def file_upload():
-#     if 'file' not in request.files:
-#         return jsonify({'error': 'No file part'}), 400
-#     file = request.files['file']
-#     if file.filename == '':
-#         return jsonify({'error': 'No selected file'}), 400
-#     if file:
-#         filename = file.filename
-#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#         return jsonify({'message': 'File uploaded successfully', 'filename': filename}), 200
+#             request_json = {
+#                 'images': [{'format': file.content_type.split('/')[1], 'name': 'demo'}],
+#                 'requestId': str(uuid.uuid4()),
+#                 'version': 'V2',
+#                 'timestamp': int(round(time.time() * 1000))
+#             }
+#             payload = {'message': json.dumps(request_json).encode('UTF-8')}
+#
+#             try:
+#                 response = requests.post(api_url, headers=headers, files=files, data=payload)
+#                 response.raise_for_status()
+#                 result = response.json()
+#                 text_results = ' '.join([field['inferText'] for field in result['images'][0]['fields']])
+#
+#                 # 바운딩 박스 그리기
+#                 for field in result['images'][0]['fields']:
+#                     infer_text = field['inferText']
+#                     vertices = field['boundingPoly']['vertices']
+#                     draw.polygon([
+#                         (vertices[0]['x'], vertices[0]['y']),
+#                         (vertices[1]['x'], vertices[1]['y']),
+#                         (vertices[2]['x'], vertices[2]['y']),
+#                         (vertices[3]['x'], vertices[3]['y'])
+#                     ], outline='red')
+#
+#                     text_position = (vertices[0]['x'], vertices[0]['y'] - 20)  # 텍스트 위치 조정 필요시 수정
+#                     draw.text(text_position, infer_text, fill=(255, 0, 0), font=font)
+#
+#                 # 이미지를 Base64로 인코딩
+#                 buffered = BytesIO()
+#                 image.save(buffered, format="JPEG")
+#                 image.show()
+#                 img_str = base64.b64encode(buffered.getvalue()).decode()
+#
+#                 return jsonify({'text': text_results, 'image': img_str})
+#
+#             except requests.exceptions.RequestException as e:
+#                 return jsonify({'error': str(e)}), 500
+#
+#     return jsonify({'error': 'No file provided'}), 400
 
 
-# 새로운 코드
+
+# 임시 코드
 @app.route('/upload', methods=['POST', 'GET'])
 def process_image():
-    if request.method == 'POST':
-        # 파일 받기
-        file = request.files['file']
-        print(file)
-        if file:
-            # API 설정
-            api_url = 'https://eoxy3e0azd.apigw.ntruss.com/custom/v1/29612/cb56e51bd632826fb10645cf94923e9934c4789df11256f9baaf17de98b0e7f9/general'
-            secret_key = 'VG1ocGpZcXlDU2xuRWFRaU1XdEtWWm1JSlZxeVV2c1A='
-            headers = {
-                'X-OCR-SECRET': secret_key
-            }
+    # 로컬 이미지 파일 경로 설정
+    file_path = "./image_test/gamja.jpg"
 
-            # 파일을 메모리에서 직접 읽어 API로 전송
-            files = {'file': (file.filename, file.stream, file.content_type)}
-            request_json = {
-                'images': [{'format': file.content_type.split('/')[1], 'name': 'demo'}],
-                'requestId': str(uuid.uuid4()),
-                'version': 'V2',
-                'timestamp': int(round(time.time() * 1000))
-            }
-            payload = {'message': json.dumps(request_json).encode('UTF-8')}
-            print(payload)
+    # API 설정
+    api_url = 'https://eoxy3e0azd.apigw.ntruss.com/custom/v1/29612/cb56e51bd632826fb10645cf94923e9934c4789df11256f9baaf17de98b0e7f9/general'
+    secret_key = 'VG1ocGpZcXlDU2xuRWFRaU1XdEtWWm1JSlZxeVV2c1A='
+    headers = {'X-OCR-SECRET': secret_key}
 
-            try:
-                # 파일과 함께 페이로드 전송
-                response = requests.post(api_url, headers=headers, files=files, data=payload)
-                response.raise_for_status()  # 요청 실패 시 예외 발생
-                result = response.json()
-                text_results = ' '.join([field['inferText'] for field in result['images'][0]['fields']])
-                print(text_results)
-                print("SUCCESS")
-                # return jsonify({'text': "SUCCESS"})
-                return jsonify({'text': text_results})
+    # 이미지 로드
+    image = Image.open(file_path)
+    draw = ImageDraw.Draw(image)
+    font_path = './Font/Korail_Round_Gothic_Light.ttf'
 
-            except requests.exceptions.RequestException as e:
-                print("FAIL")
-                print(e)
-                return jsonify({'error': str(e)}), 500
+    # 폰트 객체 생성
+    font = ImageFont.truetype(font_path, 16)
 
-    return jsonify({'error': 'No file provided'}), 400
+    # 파일을 메모리에서 직접 읽어 API로 전송
+    with open(file_path, 'rb') as file:
+        files = {'file': (file_path, file, 'image/jpeg')}
+
+        request_json = {
+            'images': [{'format': 'jpeg', 'name': 'demo'}],
+            'requestId': str(uuid.uuid4()),
+            'version': 'V2',
+            'timestamp': int(round(time.time() * 1000))
+        }
+        payload = {'message': json.dumps(request_json).encode('UTF-8')}
+
+        try:
+            response = requests.post(api_url, headers=headers, files=files, data=payload)
+            response.raise_for_status()
+            result = response.json()
+            text_results = ' '.join([field['inferText'] for field in result['images'][0]['fields']])
+
+            # 바운딩 박스 그리기
+            for field in result['images'][0]['fields']:
+                infer_text = field['inferText']
+                vertices = field['boundingPoly']['vertices']
+                draw.polygon([
+                    (vertices[0]['x'], vertices[0]['y']),
+                    (vertices[1]['x'], vertices[1]['y']),
+                    (vertices[2]['x'], vertices[2]['y']),
+                    (vertices[3]['x'], vertices[3]['y'])
+                ], outline='red')
+                # 텍스트 그리기
+                text_position = (vertices[0]['x'], vertices[0]['y'] - 20)  # 텍스트 위치 조정 필요시 수정
+                draw.text(text_position, infer_text, fill=(255, 0, 0), font=font)
+
+            # 이미지를 Base64로 인코딩
+            buffered = BytesIO()
+            image.save(buffered, format="JPEG")
+            # image.show()
+            img_str = base64.b64encode(buffered.getvalue()).decode()
+
+            return jsonify({'text': text_results, 'image': img_str})
+        except requests.exceptions.RequestException as e:
+            return jsonify({'error': str(e)}), 500
+
 
 
 if __name__ == '__main__':
